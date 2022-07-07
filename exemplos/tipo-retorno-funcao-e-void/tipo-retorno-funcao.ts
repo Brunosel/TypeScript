1600e71f5e1a89443e44bbf6464188c3004796a3
Brunosel <brunosel@hotmail.com>
2022-07-07 15:15:42 -0300

    void done

---
+++ exemplos/tipo-retorno-funcao-e-void/tipo-retorno-funcao.ts
@@ -0,0 +1,9 @@
+function somaValoresNumericos(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(somaValoresNumericos(2, 5));
+
+function printaValoresNumericos(num1: number, num2: number): void {
+    console.log(num1 + num2);
+}
--- exemplos/tipo-retorno-funcao/tipo-retorno-funcao.ts
+++
@@ -1,5 +0,0 @@
-function somaValoresNumericos(num1: number, num2: number): number {
-    return num1 + num2;
-}
-
-console.log(somaValoresNumericos(2, 5));
