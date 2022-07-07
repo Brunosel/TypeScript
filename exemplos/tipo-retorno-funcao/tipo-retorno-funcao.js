4f35ccd55e313a2bab130ce0e13efb1a5bfebf97
Brunosel <brunosel@hotmail.com>
2022-07-07 15:13:02 -0300

    tipo retorno funcao done

---
+++ exemplos/tipo-retorno-funcao/tipo-retorno-funcao.js
@@ -0,0 +1,5 @@
+"use strict";
+function somaValoresNumericos(num1, num2) {
+    return num1 + num2;
+}
+console.log(somaValoresNumericos(2, 5));
---
+++ exemplos/tipo-retorno-funcao/tipo-retorno-funcao.ts
@@ -0,0 +1,5 @@
+function somaValoresNumericos(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(somaValoresNumericos(2, 5));
